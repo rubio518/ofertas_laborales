564b2d23d813ec94bb89c9c60d84de2d618b7f60
len <rubio518@gmail.com>
2011-03-30 11:20:41 -0600

    haciendo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OfertasLaborales::Application.config.secret_token = '8a89eaf974beec24cf2f2709dc5b0196e4cdf328bdfdcb088b2de410b723009c726a0afbd3b367c4c75cad8bfccc836f7b91200840bbd44f1415e4d41526ea0c'
---
+++ db/migrate/20110311004232_scaf.rb
@@ -0,0 +1,7 @@
+class Scaf < ActiveRecord::Migration
+  def self.up
+  end
+
+  def self.down
+  end
+end
